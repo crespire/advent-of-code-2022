42a093321d5f2842acb63b6b732530037b579699
crespire <li.simmon@gmail.com>
2022-12-01 08:37:32 -0500

    feat: day 1 competed

---
+++ day1.rb
@@ -0,0 +1,21 @@
+inventory = File.open('./day1_input.txt') { |file| file.readlines(chomp: true) }
+
+elves = inventory.slice_when do |i, _|
+  i.length.zero?
+end
+
+# Part 1
+ans1 = elves.reduce(0) do |memo, elf|
+  total = elf.reduce(0) { |sum, item| sum + item.to_i }
+  memo = total > memo ? total : memo
+  memo
+end
+
+# Part 2
+ans2 = elves.map do |elf|
+  total = elf.reduce(0) { |sum, item| sum + item.to_i }
+  total
+end
+
+puts "Single elf high: #{ans1}"
+puts "Top three elves: #{ans2.sort.last(3).sum}"
